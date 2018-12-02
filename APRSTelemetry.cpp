efc599c21b2363c46c58e2241da9f9e7ae167664
Vilius Icicle <vilisas@gmail.com>
2018-12-02 14:35:26 +0200

    added bits

--- APRSTelemetry.cpp
+++ APRSTelemetry.cpp
@@ -8,71 +8,60 @@
 #include "APRSTelemetry.h"
 #include <Arduino.h>
 
-    void APRSTelemetry::updateBatteryVoltage(){
-    	updateTelemetryChannel(CH1, _battery_voltage * 100);
-    }
-
-    void APRSTelemetry::updateTemperatures(int temp1, int temp2){
-//	 reported temp = temp + 55, receiving end should use equations packet to decode.
-//	this->_temperature[0] = temp1+55;
-//	this->_temperature[1] = temp2+55;
-    	updateTelemetryChannel(CH1, temp1+80);
-    	updateTelemetryChannel(CH2, temp2+80);
-
-    }
-
-    void APRSTelemetry::updateTelemetrySequence(){
-	/*
-	 * suraso telemetrijos paketa i packet_buffer. nurodo paketo ilgi i packet_length
-	 */
-	// T#MIC199,000,255,073,123,01101001   -- 33 bytes
-//		packet_length = sprintf_P(packet_buffer, PSTR("aaa"),0);
-//	_packet_length = sprintf_P(_packet_buffer, PSTR("T#%03u,%03u,%03u,%03u,%03u,%03u,"),
-//		this->_sequence_number, this->_t_voltage, this->_temperature[0], this->_temperature[1], 0, 0);
-//	strncat(&_packet_buffer[_packet_length],_bits, 8);
-//	_packet_length +=8;
+    /**
+     * formats telemetry comment part
+     */
+    void APRSTelemetry::updateTelemetryPacket(){
 
 	// max seq number is 8191
 	(++this->_sequence_number) &= 0x1FFF;
 
-//	char encoded[3];
-//	char encoded[2];
+	updateTelemetryChannel(SEQ, _sequence_number);
+	updateTelemetryChannel(CH1, _battery_voltage * 100);
+	updateTelemetryChannel(CH2, _temperature[0]+80);
+	updateTelemetryChannel(CH3, _temperature[1]+80);
+	updateTelemetryChannel(CH4, _pressure);
+	updateTelemetryChannel(CH5, _altitude);
 
-	this->base91.encode(_telemetry_comment, _sequence_number);
-//	memcpy(&_telemetry_comment, &encoded, 2);
-
+	updateTelemetryChannel(CH_BITS, _bits);
 
     }
 
     // generate parameters packet
     void APRSTelemetry::generateParametersPacket(){
 	Serial.println(F("Generating parameters packet"));
-	_packet_length = strlen_P(_tpn);
-	strncpy_P(_packet_buffer, _tpn, _packet_length);
+	packet_length = strlen_P(_tpn);
+	strncpy_P(packet_buffer, _tpn, packet_length);
     }
 
 	//generate unit description packet
     void APRSTelemetry::generateUnitsPacket(){
 	Serial.println(F("Generating units packet"));
-	_packet_length = strlen_P(_tpu);
-	strncpy_P(_packet_buffer, _tpu, _packet_length);
+	packet_length = strlen_P(_tpu);
+	strncpy_P(packet_buffer, _tpu, packet_length);
     }
 
 //  generate equations packet
     void APRSTelemetry::generateEQNSPacket(){
 	Serial.println(F("Generating equations packet"));
-	_packet_length = strlen_P(_tpe);
-	strncpy_P(_packet_buffer, _tpe, _packet_length);
+	packet_length = strlen_P(_tpe);
+	strncpy_P(packet_buffer, _tpe, packet_length);
+    }
+
+    void APRSTelemetry::generateBITSPacket(){
+	Serial.println(F("Generating equations packet"));
+	packet_length = strlen_P(_tpb);
+	strncpy_P(packet_buffer, _tpb, packet_length);
     }
 
 //  this sends already generated packet
     void APRSTelemetry::sendTelemetryPacket(){
 
 	Serial.print(F("packet_buffer='"));
-	Serial.print(_packet_buffer);
+	Serial.print(packet_buffer);
 	Serial.println(F("'"));
 	Serial.print(F("packet_length="));
-	Serial.println(this->_packet_length);
+	Serial.println(this->packet_length);
 
 	//	call actual method (if set) to send packet
 	//	this class doesn't know how to push packet in the air, but konws what to call to do it
@@ -80,35 +69,37 @@
 			this->send_packet__();
 		}
 
-	memset(_packet_buffer,0,sizeof(_packet_buffer));
-	_packet_length=0;
+	memset(packet_buffer,0,sizeof(packet_buffer));
+	packet_length=0;
     }
 
+    // sets bit on or off
+    // using APRS defined bit numbering 1-8 (least significant bit is first on the left)
+    // 12345678
     void APRSTelemetry::setBit(uint8_t bit, bool state) {
 	if (bit - 1 < 8) {
-		_bits[bit] = (state ? '1' : '0');
+		if (state == true){
+			_bits |= 1UL << (8 - bit);
+		} else{
+			_bits &= ~(1UL << (8 - bit));
+		}
 	}
     }
 
     bool APRSTelemetry::getBit(uint8_t bit){
 	if (bit - 1 < 8) {
-	    return (_bits[bit] == '1');
+		return (_bits >> (8-bit)) & 1U;
 	}
 	return false;
     }
 
 void APRSTelemetry::updateTelemetryChannel(const TChannel channel, unsigned int value){
 	// seq, ch1 .. ch5.. digital
-	char encoded[3];
-//	char encoded[2];
 
 	if (value >8280 ){
 		value = 0;
 	}
-	this->base91.encode(&_telemetry_comment[2 + channel * 2], value);
-//	this->base91.encode(encoded, value);
-//	memcpy(&_telemetry_comment[2 + channel * 2],&encoded, 2);
-
+	this->base91.encode(&_telemetry_comment[channel * 2], value);
 }
 
 void APRSTelemetry::setup(){
@@ -122,32 +113,29 @@ void APRSTelemetry::setup(){
 void APRSTelemetry::loop(){
     unsigned int seconds = (millis() / 1000);
 
-    if (seconds - this->_ts_read_sensors > 3){
-	this->updateBatteryVoltage();
-//			telemetrija.updateTemperatures(sensors.getTempCByIndex(0), sensors.getTempCByIndex(1));
-	this->_ts_read_sensors = seconds;
-
-    }
-
-//    if (seconds - this->_ts_telemetry_packet > TELEMETRY_PACKET_INTERVAL){
-//	this->generateTelemetryPacket();
-//	this->sendTelemetryPacket();
-//
-//	this->_ts_telemetry_packet = seconds;
-//    }
-
-
     if (seconds - this->_ts_packet_to_send > TELEMETRY_PACKET_INTERVAL) {
 	switch (_packet_to_send) {
 	case 0:
-	    this->generateUnitsPacket();
+//	    this->generateUnitsPacket();
+		strncpy_P(packet_buffer, _tpu, packet_length);
+
 	    break;
 	case 1:
-	    this->generateParametersPacket();
+//	    this->generateParametersPacket();
+		strncpy_P(packet_buffer, _tpn, packet_length);
+
 	    break;
 	case 2:
-	    this->generateEQNSPacket();
+//	    this->generateEQNSPacket();
+		strncpy_P(packet_buffer, _tpe, packet_length);
+
 	    break;
+	case 3:
+//	    this->generateBITSPacket();
+		strncpy_P(packet_buffer, _tpb, packet_length);
+
+	    break;
+
 
 	}
 	++this->_packet_to_send %= 3;
